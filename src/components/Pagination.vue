9e50138eec2635782aae4be0aacece37b100a507
William Chen <seed45699@gmail.com>
2019-10-12 17:54:09 +0800

    分頁元件

---
+++ src/components/Pagination.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item" :class="{'disabled' : !propPage.has_pre}">
+          <a
+            class="page-link"
+            href="#"
+            aria-label="Previous"
+            @click.prevent="$emit('pageData',(propPage.current_page -1))"
+          >
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li
+          class="page-item"
+          v-for="page in propPage.total_pages"
+          :key="page"
+          :class="{'active': propPage.current_page === page}"
+        >
+          <a class="page-link" href="#" @click.prevent="$emit('pageData',page)">{{page}}</a>
+        </li>
+        <li class="page-item" :class="{'disabled' : !propPage.has_next}">
+          <a
+            class="page-link"
+            href="#"
+            aria-label="Next"
+            @click.prevent="$emit('pageData',(propPage.current_page +1))"
+          >
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['propPage'],
+};
+</script>
